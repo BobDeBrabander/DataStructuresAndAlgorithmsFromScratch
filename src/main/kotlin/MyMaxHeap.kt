a252af0b65bae9607c8802620e18eda345edc9ca
bdebrabander <bdebrabander@bol.com>
2021-07-23 16:25:51 +0200

    Improved testing and added HeapSort (also tested)

--- src/main/kotlin/MyMaxHeap.kt
+++ src/main/kotlin/MyMaxHeap.kt
@@ -1,14 +1,24 @@
 /**
  * Heap class done on Integers. It can be done on other objects as well. This would require to comparables.
  */
-class MyMaxHeap : MyVector<Int>() {
+class MyMaxHeap : MyVector<Int> {
 
-    fun insertElement(element: Int){
-        this.addElement(element)
-        ensureMaxHeapProperty(elementCount-1)
+    constructor() : super()
+
+    constructor(elements: Collection<Int>) : super(){
+        this.insertElements(elements)
     }
 
-    fun extractMax() : Int {
+    fun insertElement(element: Int) {
+        this.addElement(element)
+        ensureMaxHeapProperty(elementCount - 1)
+    }
+
+    fun insertElements(elements: Collection<Int>) = elements.forEach { insertElement(it) }
+
+    fun peekMax(): Int = this[0]
+
+    fun extractMax(): Int {
         val maxVal = this[0]
         this[0] = this.lastElement()
         this.removeLast()
@@ -16,18 +26,28 @@ class MyMaxHeap : MyVector<Int>() {
         return maxVal
     }
 
-    private fun parent(pos: Int) = ((pos+1) / 2) - 1
-    private fun leftChild(pos: Int)  = (2 * (pos+1)) - 1
-    private fun rightChild(pos: Int) = 2 * (pos+1)
+    fun heapSort(){
+        val originalCount = elementCount
+        for (i in elementCount-1 downTo 1){
+            swap(0, i)
+            elementCount--
+            maxHeapify(0)
+        }
+        elementCount = originalCount
+    }
 
-    private fun ensureMaxHeapProperty(pos: Int){
-        if(pos > 0 && this[parent(pos)] < this[pos]){
+    private fun parent(pos: Int) = ((pos + 1) / 2) - 1
+    private fun leftChild(pos: Int) = (2 * (pos + 1)) - 1
+    private fun rightChild(pos: Int) = 2 * (pos + 1)
+
+    private fun ensureMaxHeapProperty(pos: Int) {
+        if (pos > 0 && this[parent(pos)] < this[pos]) {
             swap(pos, parent(pos))
             ensureMaxHeapProperty(parent(pos))
         }
     }
 
-    private fun maxHeapify(pos: Int){
+    private fun maxHeapify(pos: Int) {
         val left = leftChild(pos)
         val right = rightChild(pos)
         var largest = if (left < elementCount && this[left] > this[pos]) left else pos
@@ -38,7 +58,7 @@ class MyMaxHeap : MyVector<Int>() {
         }
     }
 
-    private fun swap(pos1: Int, pos2: Int){
+    private fun swap(pos1: Int, pos2: Int) {
         val val1 = this[pos1]
         val val2 = this[pos2]
         this[pos2] = val1
--- src/test/kotlin/MyMaxHeapTester.kt
+++ src/test/kotlin/MyMaxHeapTester.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertEquals
 class MyMaxHeapTester {
 
     @Test
-    fun `Basic MaxHeap test`(){
+    fun `Inserting a series of elements and extracting them one by one`() {
         val maxHeap = MyMaxHeap()
         maxHeap.insertElement(5)
         maxHeap.insertElement(12)
@@ -17,5 +17,46 @@ class MyMaxHeapTester {
         assertEquals(7, maxHeap.extractMax(), "Not expected max")
         assertEquals(5, maxHeap.extractMax(), "Not expected max")
         assertEquals(3, maxHeap.extractMax(), "Not expected max")
+    }
+
+    @Test
+    fun `Inserting a lot of elements in one go, and extracting them one by one`() {
+        val maxHeap = MyMaxHeap()
+        maxHeap.insertElements(listOf(5, 12, 3, 18, 7))
+        assertEquals(18, maxHeap.extractMax(), "Not expected max")
+        assertEquals(12, maxHeap.extractMax(), "Not expected max")
+        assertEquals(7, maxHeap.extractMax(), "Not expected max")
+        assertEquals(5, maxHeap.extractMax(), "Not expected max")
+        assertEquals(3, maxHeap.extractMax(), "Not expected max")
+    }
+
+    @Test
+    fun `Constructing a maxHeap from a collection of elements, and extracting every element one by one`() {
+        val maxHeap = MyMaxHeap(listOf(5, 12, 3, 18, 7))
+        assertEquals(18, maxHeap.extractMax(), "Not expected max")
+        assertEquals(12, maxHeap.extractMax(), "Not expected max")
+        assertEquals(7, maxHeap.extractMax(), "Not expected max")
+        assertEquals(5, maxHeap.extractMax(), "Not expected max")
+        assertEquals(3, maxHeap.extractMax(), "Not expected max")
+    }
+
+    @Test
+    fun `PeekMax works`() {
+        val maxHeap = MyMaxHeap()
+        maxHeap.insertElements(listOf(5, 12, 3, 18, 7))
+        assertEquals(18, maxHeap.peekMax(), "Not expected max")
+    }
+
+    @Test
+    fun `HeapSort works`() {
+        val maxHeap = MyMaxHeap()
+        maxHeap.insertElements(listOf(5, 12, 3, 18, 7, 21, 4, 95, 34, 2, 0, -6, 45))
+        maxHeap.heapSort()
+        assertEquals(
+                listOf(-6, 0, 2, 3, 4, 5, 7, 12, 18, 21, 34, 45, 95),
+                maxHeap.toList(),
+                "MaxHeap not correctly sorted"
+        )
+
     }
 }
